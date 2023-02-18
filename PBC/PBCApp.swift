86f27c1c6979d95adbe217b772a8e21b4557e8d0
Cheon, Seongwoo <cjs1399@naver.com>
2023-02-18 20:11:01 +0900

    Initial Commit

---
+++ PBC/PBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PBCApp.swift
+//  PBC
+//
+//  Created by 천성우 on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct PBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
